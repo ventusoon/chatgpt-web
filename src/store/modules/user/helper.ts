a2eb8cd0742bc53805d63cf31cadb22cecbe5ae2
Ventus <sia@ik.me>
2023-03-10 23:59:00 +0800

    Add files via upload

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,8 +15,8 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
+      avatar: 'https://github.com/ventusoon/LuvSia/raw/gh-pages/images/avatar.png',
+      name: 'ven2s',
       description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
