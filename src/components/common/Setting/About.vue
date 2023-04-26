c956d76c8f22f8c5882911fa6182dab2c5ae178e
ven2s <ven2s@ven2s.local>
2023-04-26 12:27:22 +0800

    个性化设置

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -42,14 +42,14 @@ onMounted(() => {
   <NSpin :show="loading">
     <div class="p-4 space-y-4">
       <h2 class="text-xl font-bold">
-        Version - {{ pkg.version }}
+        Version - {{ pkg.version }} - ven2s个人版
       </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
           此项目开源于
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="https://github.com/ventusoon/chatgpt-web"
             target="_blank"
           >
             GitHub
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://luvsia.com/images/avatar.png',
+      name: 'ven2s',
+      description: '<a href="https://luvsia.com/" class="text-blue-500" target="_blank" >风至万始苏</a>',
     },
   }
 }
